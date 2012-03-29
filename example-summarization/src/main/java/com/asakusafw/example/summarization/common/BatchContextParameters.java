05134eabdc70f9c270742abcb543b186ceb5d5b6
Hidekazu Suga <suga@nautilus-technologies.com>
2012-03-29 17:58:47 +0900

    Added Example Summarization

---
+++ example-summarization/src/main/java/com/asakusafw/example/summarization/common/BatchContextParameters.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2012 Asakusa Framework Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asakusafw.example.summarization.common;
+
+import com.asakusafw.runtime.core.BatchContext;
+import com.asakusafw.runtime.value.DateTime;
+import com.asakusafw.runtime.value.DateTime.Format;
+
+/**
+ * 処理対象発行日を対象とした{@link BatchContext}のwrapper.
+ */
+public class BatchContextParameters {
+
+	/**
+	 * 発行開始日のパラメータ名
+	 */
+	public static final String FROM_ISSUE_DATE = "FROM_ISSUE_DATE";
+
+	/**
+	 * 発行終了日のパラメータ名
+	 */
+	public static final String TO_ISSUE_DATE = "TO_ISSUE_DATE";
+
+	/**
+	 * バッチパラメータより発行開始日を取得する.
+	 * <p>
+	 * 時刻は00時00分00秒が設定される
+	 * </p>
+	 *
+	 * @return 発行開始日
+	 */
+	public DateTime getFromIssueDate() {
+		String fromIssueDate = getIssueDate(FROM_ISSUE_DATE);
+		return DateTime.valueOf(fromIssueDate + "000000", Format.SIMPLE);
+	}
+
+	/**
+	 * バッチパラメータより発行終了日を取得する.
+	 * <p>
+	 * 時刻は23時59分59秒が設定される
+	 * </p>
+	 *
+	 * @return 発行終了日
+	 */
+	public DateTime getToIssueDate() {
+		String toIssueDate = getIssueDate(TO_ISSUE_DATE);
+		return DateTime.valueOf(toIssueDate + "235959", Format.SIMPLE);
+	}
+
+	/**
+	 * {@link BatchContext} から日付の文字列表現を取得する.
+	 *
+	 * @param parameterName
+	 *            {@link BatchContext} の変数名
+	 * @return 日付の文字列表現
+	 * @exception IllegalArgumentException
+	 *                パラメータの値が8桁の数値以外
+	 */
+	private String getIssueDate(String parameterName) {
+		String dateAsString = BatchContext.get(parameterName);
+		if (dateAsString == null || dateAsString.length() != 8
+				|| "[0-9]*".matches(dateAsString)) {
+			throw new IllegalArgumentException(dateAsString
+					+ " is not valid parameter for " + parameterName);
+		}
+		return dateAsString;
+	}
+
+}
---
+++ example-summarization/src/main/java/com/asakusafw/example/summarization/operator/SummarizeReceiptOperator.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2012 Asakusa Framework Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asakusafw.example.summarization.operator;
+
+import com.asakusafw.example.summarization.modelgen.dmdl.model.AllValues;
+import com.asakusafw.vocabulary.model.Key;
+import com.asakusafw.vocabulary.operator.Fold;
+
+/**
+ * 単品別、カテゴリ別、店舗別にレシートデータを集計する演算子
+ */
+public abstract class SummarizeReceiptOperator {
+
+	/**
+	 * 商品コード別にレシートデータを集計する
+	 * @param sum 集計結果レコード
+	 * @param each 集計対象レコード
+	 */
+	@Fold
+	public void summarizeItem(@Key(group = "jan_code", order = "receipt_id ASC") AllValues sum, AllValues each) {
+		addValues(sum, each);
+	}
+
+	/**
+	 * 店舗コード別にレシートデータを集計する
+	 * @param sum 集計結果レコード
+	 * @param each 集計対象レコード
+	 */
+	@Fold
+	public void summarizeShop(@Key(group = "shop_code", order = "receipt_id ASC") AllValues sum, AllValues each) {
+		addValues(sum, each);
+	}
+
+	/**
+	 * カテゴリコード別にレシートデータを集計する
+	 * @param sum 集計結果レコード
+	 * @param each 集計対象レコード
+	 */
+	@Fold
+	public void summarizeCategory(@Key(group = "category_code", order = "receipt_id ASC") AllValues sum, AllValues each) {
+		addValues(sum, each);
+	}
+
+	/**
+	 * 値を集計結果に追加する
+	 * @param sum 集計結果レコード
+	 * @param each 集計対象レコード
+	 */
+	private void addValues(AllValues sum, AllValues each) {
+		if (!sum.getReceiptIdOption().equals(each.getReceiptIdOption())) {
+			sum.setCustomers(sum.getCustomers() + 1);
+			sum.setReceiptId(each.getReceiptId());
+		}
+		sum.setUnitSales(sum.getUnitSalesOption().or(0) + each.getUnitSalesOption().or(0));
+		sum.setSellingPrice(sum.getSellingPriceOption().or(0) + each.getSellingPriceOption().or(0));
+	}
+
+}
